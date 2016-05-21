0abb62c71c8927dccbcf4bd590dc9e32133eb50b
Kanit Srisuthep <kanit9618@gmail.com>
2016-05-21 15:22:00 -0700

    add react prototype

---
+++ index.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// var SDK = require('inline-sdk');
+// var CommandType = SDK.CommandType;
+// var inline = new SDK();
+
+// inline.onCommand((type, payload, context) => {
+//   if (type !== CommandType.MESSAGE) return;
+
+//   // context.sendText(payload.args.join(' '));
+//   // console.log(payload.args.length);
+
+// });
+
